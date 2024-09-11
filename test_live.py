7a5dbdb098dc46edcf2aab7f00b1cd41396359b3
Iroshani Jayawardene <iroshanij@Iroshanis-MacBook-Pro.local>
2024-09-11 15:14:05 +0200

    modified test_live

--- test_live.py
+++ test_live.py
@@ -8,20 +8,13 @@ from PIL import ImageColor, Image
 import yaml
 import random
 
-import cv2
-from ultralytics import YOLO
-import argparse
-from PIL import ImageColor, Image
-import yaml
-import random
 
 # Read the YAML file
-file_path = "GU_11:23:54.yaml"
+file_path = "GU_11:23:54_v2.yaml"
 with open(file_path, 'r') as file:
     data = yaml.safe_load(file)
 
 # Extract names and assign a color for each
-# Assuming 'names' contains the species names
 names = data['names']
 
 # Define fixed colors
@@ -35,109 +28,77 @@ for name, color in colors.items():
     print(f"Species: {name}, Color: {color}")
 
 
-
-# write a function to test the model on a each frame of the video and return the output video
-# input: video path, output video path, model path
-# output: output video path
-# load the model
-
-def test_video(video_path,  model):
-
-    cap = cv2.VideoCapture(video_path)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    codec = int(cap.get(cv2.CAP_PROP_FOURCC))
-    # save as .mp4
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # split the file name from / and then split the file name from .
-    #out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-    print("Processing video...", video_path)
-
-    # loop through the video frames
+def test_video(video_url, model):
+    # Capture the video stream
     count = 0
+    cap = cv2.VideoCapture(video_url)
+    print("Processing video stream...")
+
     while cap.isOpened():
+        count = count + 1
         ret, frame = cap.read()
-       
+
         if not ret:
+            print("Stream ended or connection failed")
             break
+        if count % 3 == 0:
+            # Detect objects in the frame using YOLO model
+            results = model(frame)
 
-
-        # detect the objects in the images
-        #resized = cv2.resize(image, (640, 640))
-        results = model(frame)
-        for result in results:
+            for result in results:
                 objects = result.boxes.cpu().numpy()
                 for j, box in enumerate(objects):
                     r = box.xyxy[0].astype(int)
-                    # scale bounding box coordinates to the original image size
                     confidence = box.conf[0].astype(float)
                     class_id = box.cls[0].astype(int)
                     label = names[class_id]
                     color = colors.get(label, "Color not assigned")
-                    # Convert the color string to a BGR tuple
+                    
+                    # Convert color string to BGR tuple
                     rgb_color = ImageColor.getcolor(color, "RGB")
                     bgr_color = (rgb_color[2], rgb_color[1], rgb_color[0])
                     text = f"{label} {confidence:.2f}"
-                    #if confidence > 0.65:
-                    cv2.rectangle(frame, (r[0], r[1]), (r[2], r[3]), bgr_color, 2)
-                    font_scale = 0.5  # Smaller font size
-                    thickness = 1     # Adjust thickness as needed
-                    cv2.putText(frame, text, (r[0], r[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, bgr_color, thickness)
-                    # save the image
-                    if confidence > 0.75:
-                        file_name = './test_images/'+ str(count)+'.jpg'
-                        cv2.imwrite(file_name,frame)
+                    
+                    # Draw bounding box and label if confidence is above the threshold
+                    if confidence > 0.20:
+                        cv2.rectangle(frame, (r[0], r[1]), (r[2], r[3]), bgr_color, 2)
+                        font_scale = 0.5
+                        thickness = 1
+                        cv2.putText(frame, text, (r[0], r[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, bgr_color, thickness)
 
-        # Display the frame
-        cv2.imshow('YOLO Object Detection', frame)
+            # Display the frame with YOLO detections
+            cv2.imshow('YOLO Object Detection', frame)
 
-        # Exit if 'q' is pressed
+        # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        
-        #print(f"Detected {label} with confidence {confidence:.2f}")
-        count = count + 1
-        
-        # write the frame to the output video
-        #out.write(frame)
-        # Release the video capture and close all windows
-        #capture.release()
-        #cv2.destroyAllWindows()
-
-
-    # release the video
+    # Release resources
     cap.release()
-    #out.release()
     cv2.destroyAllWindows()
 
-    #return output_path
 
 def main():
-    #video_path = "../clip2.mp4"
-    # URL of the YouTube video
+    # URL of the YouTube video stream
     url = "https://www.youtube.com/watch?v=ssiZXK7mvJM"
 
-    # Extract video URL using yt-dlp
+    # Extract video stream URL using yt-dlp
     ydl_opts = {'format': 'best', 'noplaylist': True}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         video_url = info_dict['formats'][0]['url']
 
-    # Get the HLS stream using streamlink
+    # Use streamlink to get the best HLS stream
     streams = streamlink.streams(url)
     stream = streams["best"]
 
-    model_path = "models/weights/best.pt"
+    # Load the YOLO model
+    model_path = "models/weights_v2/best.pt"
     model = YOLO(model_path)
+
+    # Test the YOLO model on the real-time video stream
     test_video(stream.url, model)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
